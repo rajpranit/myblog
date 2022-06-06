64b542ee0d6940577466d4a41d335aa894e9aef6
rajpranit <rajpranit1207@gmail.com>
2022-06-06 13:16:46 +0530

    test6

--- myblog/settings.py
+++ myblog/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
