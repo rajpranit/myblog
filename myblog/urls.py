79b8b39bbec8950b4c7eabc75b1ef8939684cd06
rajpranit <rajpranit1207@gmail.com>
2022-06-06 18:36:16 +0530

    'staticfiles1'

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('weebs.urls'))
-]+static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+    path('',include('weebs.urls')),
+    re_path(r'^media/(?P<path>,*)$',serve,{'document_root': settings.MEDIA_ROOT},)
+    
+]
+
+urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL , document_root=settings.STATICFILES_DIRS)
