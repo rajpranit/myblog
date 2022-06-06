442064afaa05d2bcc7ea735149dc18964efbaba9
rajpranit <rajpranit1207@gmail.com>
2022-06-06 17:40:54 +0530

    'whitenoise'

--- myblog/settings.py
+++ myblog/settings.py
@@ -3,7 +3,7 @@ import django_heroku
 import dj_database_url
 from decouple import config
 import os
-
+from whitenoise import WhiteNoise
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
